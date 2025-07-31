b9f5f8af29cddfff7722157dfd3f3da116f715a0
Ivan Dudka <tovremya@gmail.com>
2025-07-31 16:24:47 +0300

    changes for review

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '89.169.182.227', 'yourtaski.zapto.org']
+ALLOWED_HOSTS = ['localhost', '89.169.182.227', '127.0.0.1', 'yourtaski.zapto.org']
 
 
 # Application definition
